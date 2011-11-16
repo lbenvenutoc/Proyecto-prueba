aa508beec51ad771938b98b8cb6996abe9f45f58
lsc.sistemas.jpa@gmail.com <lsc.sistemas.jpa@gmail.com@3131285c-e338-9836-22af-46756a3415e7>
2011-11-16 01:13:38 +0000

    Generando EmpresaDaoImp

---
+++ Bastanteo_Cloud/src/bastanteo/cloud/dao/EmpresaDaoImp.java
@@ -0,0 +1,18 @@
+package bastanteo.cloud.dao;
+
+import org.hibernate.Session;
+
+import bastanteo.cloud.bean.Empresa;
+import bastanteo.cloud.util.HibernateUtil;
+
+public class EmpresaDaoImp implements EmpresaDao {
+
+	@Override
+	public Empresa obtenerEmpresa(Empresa objEmpresa) {
+		Session sesion=HibernateUtil.getSessionFactory();
+		Empresa objEmpresaObt=null;
+		objEmpresaObt=(Empresa)sesion.load(Empresa.class, objEmpresa.getCEmpresa());
+		return objEmpresaObt;
+	}
+
+}
