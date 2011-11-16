7adea5272da298bc2199bd85de4e0c86c708202c
lsc.sistemas.jpa@gmail.com <lsc.sistemas.jpa@gmail.com@3131285c-e338-9836-22af-46756a3415e7>
2011-11-16 01:37:07 +0000

    Creando el dao de grupo de bastanteo para obtener el grupo y vincularlo a un representante

---
+++ Bastanteo_Cloud/src/bastanteo/cloud/dao/GrupoBastanteoDao.java
@@ -0,0 +1,9 @@
+package bastanteo.cloud.dao;
+
+import bastanteo.cloud.bean.GrupoBastanteo;
+
+public interface GrupoBastanteoDao {
+	
+	public GrupoBastanteo obtenerGrupoBastanteo(GrupoBastanteo objGrupoBastanteo);
+
+}
---
+++ Bastanteo_Cloud/src/bastanteo/cloud/dao/GrupoBastanteoDaoImp.java
@@ -0,0 +1,19 @@
+package bastanteo.cloud.dao;
+
+import org.hibernate.Session;
+
+import bastanteo.cloud.bean.GrupoBastanteo;
+import bastanteo.cloud.bean.Poder;
+import bastanteo.cloud.util.HibernateUtil;
+
+public class GrupoBastanteoDaoImp implements GrupoBastanteoDao {
+
+	
+	public GrupoBastanteo obtenerGrupoBastanteo(GrupoBastanteo objGrupoBastanteo) {
+		Session session = HibernateUtil.getSessionFactory();
+		GrupoBastanteo objGrupo=null;
+		objGrupo=(GrupoBastanteo)session.load(GrupoBastanteo.class, objGrupoBastanteo.getId());
+		return objGrupo;
+	}
+
+}
