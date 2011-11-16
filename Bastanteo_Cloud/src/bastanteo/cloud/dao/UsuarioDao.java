24ebb97aad730378f7e5ebf4b2fb54cb5e011b86
geraldine.onofre@gmail.com <geraldine.onofre@gmail.com@3131285c-e338-9836-22af-46756a3415e7>
2011-11-16 00:37:05 +0000

    Viendo los ultimos cambios

--- Bastanteo_Cloud/src/bastanteo/cloud/dao/UsuarioDao.java
+++ Bastanteo_Cloud/src/bastanteo/cloud/dao/UsuarioDao.java
@@ -5,7 +5,7 @@ import bastanteo.cloud.bean.Acceso;
 import bastanteo.cloud.bean.AccesoId;
 
 public interface UsuarioDao {
-	
+	//CREANDO INTERFACES
 	public int controlarUsuario(Usuario objusuario);
 	public int modificarUsuario(Usuario objusuario);
 	public Acceso obtenerAcceso(AccesoId objAccesoId);
