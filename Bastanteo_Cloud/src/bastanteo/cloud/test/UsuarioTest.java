7ee8bf99c2deba7a49ea966001582b5e05944eac
geraldine.onofre@gmail.com <geraldine.onofre@gmail.com@3131285c-e338-9836-22af-46756a3415e7>
2011-11-16 00:59:40 +0000

    Ya funciona el test Usuario

--- Bastanteo_Cloud/src/bastanteo/cloud/test/UsuarioTest.java
+++ Bastanteo_Cloud/src/bastanteo/cloud/test/UsuarioTest.java
@@ -3,19 +3,45 @@ package bastanteo.cloud.test;
 import org.junit.Test;
 
 import junit.framework.Assert;
+import bastanteo.cloud.bean.Perfil;
 import bastanteo.cloud.bean.Usuario;
+import bastanteo.cloud.service.PerfilService;
 import bastanteo.cloud.service.UsuarioService;
 
 public class UsuarioTest {
-	@Test
+	
+	//@Test
 	public void controlarUsuarioTest() {
 
 		int retorno = 0;
 		
 		UsuarioService servicioUsuario = new UsuarioService();
-		Usuario objusuario = new Usuario(1, 1,"1234","Gery","Geraldine", "Onofre", "Gonzales",44579887,"A");
-
+		PerfilService servicioPerfil= new PerfilService();
+		//Inicializo un perfil
+		Perfil p= new Perfil();
+		//Inicializo un perfil con el codigo 1 q ya existe
+		p.setCPerfil(1);
+		Perfil objPerfil=servicioPerfil.obtenerPerfil(p);
+		Usuario objusuario = new Usuario(1, objPerfil,"1234","Gery","Geraldine", "Onofre", "Gonzales","44579887",'A');
+		
 		retorno = servicioUsuario.ControlarUsuario(objusuario);
 		Assert.assertEquals(1, retorno);
 	}
+	
+	@Test
+	public void modificarUsuarioTest() {
+
+		int retorno = 0;
+		
+		UsuarioService servicioUsuario = new UsuarioService();
+		Usuario objUsuario = new Usuario();
+		objUsuario.setCUsuario(1);
+		Usuario objUsuarioObt=servicioUsuario.obtenerUsuario(objUsuario);
+		//objUsuarioObt.setLogin("GERAL");
+		objUsuarioObt.setNombre("Lisseth");
+		
+		retorno = servicioUsuario.modificarUsuario(objUsuarioObt);
+		Assert.assertEquals(1, retorno);
+	}
+	
 }
