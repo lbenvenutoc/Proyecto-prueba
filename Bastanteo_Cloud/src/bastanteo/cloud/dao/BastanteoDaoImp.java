a91193b0e7f14bd05f00a0d237cb8e67eb2cad55
rickraf.01@gmail.com <rickraf.01@gmail.com@3131285c-e338-9836-22af-46756a3415e7>
2011-11-17 06:12:32 +0000

    Modificación de la clase bastanteo.

--- Bastanteo_Cloud/src/bastanteo/cloud/dao/BastanteoDaoImp.java
+++ Bastanteo_Cloud/src/bastanteo/cloud/dao/BastanteoDaoImp.java
@@ -1,7 +1,12 @@
 package bastanteo.cloud.dao;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.transform.AliasToEntityMapResultTransformer;
 
 import bastanteo.cloud.bean.Bastanteo;
 import bastanteo.cloud.bean.BastanteoId;
@@ -11,9 +16,64 @@ import bastanteo.cloud.util.HibernateUtil;
 
 public class BastanteoDaoImp implements BastanteoDao {
 
+	private int verificarBastanteo(Bastanteo objBastanteo) {
+		int retorno = 0;
+		
+		Session session = HibernateUtil.getSessionFactory();
+		Transaction tx = null;
+		List lstResultado = new ArrayList();
+		String select = "";
+		Query query = null;		
+				
+		try {
+			tx = session.beginTransaction();
+			
+			select = "select c_empresa from bastanteo where c_empresa = :c_empresa and c_grupo_bastanteo = :c_grupo_bastanteo and c_poder = :c_poder and c_tipo_intervencion = :c_tipo_intervencion;";
+			
+			query = session.createSQLQuery(select).addScalar("C_EMPRESA")
+					.setResultTransformer(
+							AliasToEntityMapResultTransformer.INSTANCE);
+			
+			query.setString("c_empresa", objBastanteo.getId().getCEmpresa());
+			query.setInteger("c_grupo_bastanteo", objBastanteo.getId().getCGrupoBastanteo());
+			query.setString("c_poder", objBastanteo.getId().getCPoder());
+			query.setInteger("c_tipo_intervencion", objBastanteo.getTipoIntervencion().getCTipoIntervencion());
+			
+			lstResultado = query.list();
+			
+			if(lstResultado.size() > 0) {
+				retorno = -1;
+			}else {
+				retorno = 1;
+			}
+			
+			tx.commit();
+
+		} catch (Exception e) {
+			if (tx != null)
+				tx.rollback();
+			try {
+				throw e;
+			} catch (Exception e1) {
+
+				e1.printStackTrace();
+			}
+			retorno = 0;
+		} finally{
+			session.close();
+		}
+		
+		return retorno;
+	}
+	
 	public int insertarBastanteo(Bastanteo objBastanteo) {
 		
-		int li_retorno;
+		int retorno;
+		
+		retorno = verificarBastanteo(objBastanteo);
+		if(retorno != 0){
+			return retorno;
+		}
 		
 		Session session = HibernateUtil.getSessionFactory();
 		Transaction tx = null;
@@ -22,17 +82,19 @@ public class BastanteoDaoImp implements BastanteoDao {
 			tx = session.beginTransaction();
 			session.save(objBastanteo);
 			tx.commit();
-			li_retorno = 1;
+			retorno = 1;
 		} catch (Exception ex) {
+			System.out.println(ex.getMessage());
 			tx.rollback();
-			li_retorno = 0;
+			retorno = 0;
 		} finally {
 			session.close();
 		}
 		
-		return li_retorno;
+		return retorno;
 	}
 
+	
 	public int actualizarBastanteo(Bastanteo objBastanteo) {
 		// TODO Auto-generated method stub
 		return 0;
--- Bastanteo_Cloud/src/bastanteo/cloud/test/BastanteoTest.java
+++ Bastanteo_Cloud/src/bastanteo/cloud/test/BastanteoTest.java
@@ -1,7 +1,13 @@
 package bastanteo.cloud.test;
 
-import java.sql.Date;
+import static org.junit.Assert.*;
+
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+
+import junit.framework.Assert;
 
 import org.junit.Test;
 
@@ -14,30 +20,36 @@ import bastanteo.cloud.service.BastanteoService;
 public class BastanteoTest {
 
 	
-
-	@Test
-	public void insertarBastanteo() {
+	@Test 
+	public void insertarBastanteo() throws ParseException {
 		
 		int retorno;
+		Date fechaIni;
+		Date fechaFin;
+		
 		BastanteoService bastanteoService = new BastanteoService();
 		Bastanteo bastanteo = new Bastanteo();
 		BastanteoId bastanteoId = new BastanteoId();
-		SimpleDateFormat sdf = new SimpleDateFormat();
+		DateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 		
 		// codigo del bastanteo
 		bastanteoId.setCEmpresa("100");
 		bastanteoId.setCGrupoBastanteo(1);
 		bastanteoId.setCPoder("CHCO");
 		
+		
+		fechaIni = sdf.parse("2011-02-01");
+		fechaFin = sdf.parse("2011-02-01");
+		
 		bastanteo.setId(bastanteoId);
 		bastanteo.setTipoIntervencion(new TipoIntervencion(100, ""));
 		bastanteo.setImporteMin(100);
 		bastanteo.setImporteMax(1000);
-		//bastanteo.setFechaIni(sdf.parse("2011-02-01"));
-		//bastanteo.setFechaFin(sdf.parse("2011-02-01"));
+		bastanteo.setFechaIni(fechaIni);
+		bastanteo.setFechaFin(fechaFin);
 		
-		
-		retorno = bastanteoService.insertarBastanteo(bastanteo);
+		//retorno = bastanteoService.insertarBastanteo(bastanteo);
+		assertEquals(1, bastanteoService.insertarBastanteo(bastanteo));
 		
 	}
 }
