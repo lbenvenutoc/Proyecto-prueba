b8ec5a1dcdc243eb6585655d2335f081d5b5ee79
lbenvenutoc <lbenvenutoc@3131285c-e338-9836-22af-46756a3415e7>
2011-11-18 03:22:07 +0000

    Empresa test

---
+++ Bastanteo_Cloud/src/bastanteo/cloud/test/EmpresaTest.java
@@ -0,0 +1,62 @@
+package bastanteo.cloud.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import bastanteo.cloud.bean.Empresa;
+import bastanteo.cloud.bean.TipoEmpresa;
+import bastanteo.cloud.service.EmpresaService;
+import bastanteo.cloud.service.TipoEmpresaService;
+import bastanteo.cloud.util.Utilitario;
+
+public class EmpresaTest {
+	//@Test
+	public void registrarEmpresa(){
+		
+		int resultado=0;
+		EmpresaService servicioEmpresa= new EmpresaService();
+		TipoEmpresaService servicioTipoEmpresa= new TipoEmpresaService();
+		TipoEmpresa objTipoEmpresa= new TipoEmpresa();
+		objTipoEmpresa.setCTipoEmpresa(1);
+		TipoEmpresa tipoEmpresa=  servicioTipoEmpresa.obtenerTipoEmpresa(objTipoEmpresa);
+		Utilitario objUtil=Utilitario.crearUtilitario();
+		
+		Empresa objEmpresa= 
+			new Empresa("2", tipoEmpresa, "21526423628", "ASOCIADOS SAC", objUtil.convertirCadenaPorFecha("08/09/2011"), 178);
+			
+		
+		Empresa objEmpresaObt=servicioEmpresa.obtenerEmpresa(objEmpresa);
+		Empresa objEmpresaObtRuc=servicioEmpresa.obtenerEmpresaxRuc(objEmpresa);
+		
+		if(objEmpresa.getCEmpresa()==null || objEmpresa.getRuc()==null || objEmpresa.getTipoEmpresa()==null || objEmpresa.getCantEmpleado()==0 || objEmpresa.getFechaInicioOper()==null ||  objEmpresa.getRazonSocial()==null){
+			System.out.println("FALTAN INGRESAR DATOS");
+		}else if(objEmpresaObt!=null){
+			System.out.println("LA EMPRESA YA EXISTE CON DICHO CODIGO");
+		}else if(objEmpresaObtRuc!=null){
+			System.out.println("EL RUC DE LA EMPRESA YA ESTA REGISTRADO");
+		}else{
+			resultado = servicioEmpresa.registrarEmpresa(objEmpresa);
+			Assert.assertEquals(resultado,1);
+			System.out.println("EMPRESA REGISTRADA CORRECTAMENTE");
+		}
+		
+		
+	}
+	
+	@Test
+	public void actualizarEmpresa(){
+		
+		int resultado=0;
+		EmpresaService servicioEmpresa= new EmpresaService();
+		Empresa objEmpresa= new Empresa();
+		objEmpresa.setCEmpresa("2");
+		Empresa objEmpresaObt=servicioEmpresa.obtenerEmpresa(objEmpresa);
+		System.out.println("ACTUAL RAZON SOCIAL "+objEmpresaObt.getRazonSocial());
+		objEmpresaObt.setRazonSocial("ASOCIADOS SAC");
+		resultado=servicioEmpresa.modificarEmpresa(objEmpresaObt);
+		Assert.assertEquals(resultado,1);
+		
+	}
+	
+
+}
