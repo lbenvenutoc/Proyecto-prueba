a30bfb2d9a32e0ec69b41637810f926c6f9e4751
lbenvenutoc@gmail.com <lbenvenutoc@gmail.com@3131285c-e338-9836-22af-46756a3415e7>
2011-12-01 21:37:55 +0000

    Mostrar mensajes de valiDACION EN EL ACTION Y SE CORRIGIO EL VINCULO CON LOS COMBOS

--- Bastanteo_Cloud/src/bastanteo/cloud/action/EmpresaAction.java
+++ Bastanteo_Cloud/src/bastanteo/cloud/action/EmpresaAction.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.faces.model.SelectItem;
 
 import bastanteo.cloud.bean.Empresa;
@@ -11,151 +13,124 @@ import bastanteo.cloud.bean.TipoEmpresa;
 import bastanteo.cloud.service.EmpresaService;
 import bastanteo.cloud.service.TipoEmpresaService;
 
+public class EmpresaAction implements Serializable {
 
-
-
-
-
-public class EmpresaAction implements  Serializable{
-	
-	
-	EmpresaService servicioEmpresa= new EmpresaService();
-	TipoEmpresaService servicioTipoEmpresa= new TipoEmpresaService();
+	EmpresaService servicioEmpresa = new EmpresaService();
+	TipoEmpresaService servicioTipoEmpresa = new TipoEmpresaService();
 	private List<TipoEmpresa> lstTipoEmpresa = new ArrayList();
 	private Empresa objEmpresa;
 	private String codEmpresa;
 	private List<Empresa> lstEmpresa;
 	private int codTipoEmpresa;
-	
-	
-	
-
-	
-
-
-
-
-
 
 	public int getCodTipoEmpresa() {
 		return codTipoEmpresa;
 	}
 
-
-
 	public void setCodTipoEmpresa(int codTipoEmpresa) {
 		this.codTipoEmpresa = codTipoEmpresa;
 	}
-
-
 
 	public String getCodEmpresa() {
 		return codEmpresa;
 	}
 
-
-
 	public void setCodEmpresa(String codEmpresa) {
 		this.codEmpresa = codEmpresa;
 	}
-
-
 
 	public Empresa getObjEmpresa() {
 		return objEmpresa;
 	}
 
-	
-	
 	public void setObjEmpresa(Empresa objEmpresa) {
 		this.objEmpresa = objEmpresa;
 	}
 
-
-	
-	
-	
-	
-	
-	
 	public List<Empresa> getLstEmpresa() {
 		return lstEmpresa;
 	}
 
-
 	public void setLstEmpresa(List<Empresa> lstEmpresa) {
 		this.lstEmpresa = lstEmpresa;
 	}
-	
 
-	public String muestraEmpresa(){
-		
-	    lstTipoEmpresa=servicioTipoEmpresa.listar();
-	    
+	public String muestraEmpresa() {
+		objEmpresa = new Empresa();
+		lstTipoEmpresa = servicioTipoEmpresa.listar();
+
 		return "muestraEmpresa";
 	}
-	
-	public String muestraEdicionEmpresa(){
-		Empresa objEmpresaEnviado= new Empresa();
-		System.out.println("CODIGO ENVIADO EMPRESA "+codEmpresa);
+
+	public String muestraEdicionEmpresa() {
+
+		Empresa objEmpresaEnviado = new Empresa();
+
 		objEmpresaEnviado.setCEmpresa(codEmpresa);
-		objEmpresa=servicioEmpresa.obtenerEmpresa(objEmpresaEnviado);
-		System.out.println("AAAAA "+objEmpresa.getTipoEmpresa().getNombre());
+		objEmpresa = servicioEmpresa.obtenerEmpresa(objEmpresaEnviado);
+
 		return "muestraEdicionEmpresa";
 	}
 
+	public String listaEmpresa() {
 
-	public String listaEmpresa(){
-		
-		lstEmpresa=servicioEmpresa.listar();
-		//lstTipoEmpresa=servicioTipoEmpresa.listar();
+		lstEmpresa = servicioEmpresa.listar();
+		// lstTipoEmpresa=servicioTipoEmpresa.listar();
 		return "listaEmpresa";
 	}
-	
-	public String registraEmpresa(){
+
+	public String registraEmpresa() {
+		String retorno="";
 		System.out.println("ENTRA  A REGISTRA EMPRESA");
-				
-		TipoEmpresa objTipEmpObt= servicioTipoEmpresa.obtenerTipoEmpresa(codTipoEmpresa);
-		objEmpresa.setTipoEmpresa(objTipEmpObt);
-		servicioEmpresa.registrarEmpresa(objEmpresa);
-		System.out.println(""+objEmpresa.getCantEmpleado());
-		lstEmpresa=servicioEmpresa.listar();
-		return "listaEmpresa";
+
+		if (servicioEmpresa.obtenerEmpresa(objEmpresa) == null) {
+			TipoEmpresa objTipEmpObt = servicioTipoEmpresa
+					.obtenerTipoEmpresa(codTipoEmpresa);
+			objEmpresa.setTipoEmpresa(objTipEmpObt);
+			servicioEmpresa.registrarEmpresa(objEmpresa);
+
+			lstEmpresa = servicioEmpresa.listar();
+			retorno="listaEmpresa";
+		} else {
+			FacesMessage msg = new FacesMessage(
+					FacesMessage.SEVERITY_ERROR,
+					"Error al ingresar empresa, ya que el código ya existe",
+					null);
+			FacesContext.getCurrentInstance().addMessage(null, msg);
+			retorno="muestraEmpresa";
+		}
+
+		return retorno;
 	}
-	
-	public String actualizaEmpresa(){
-		
-		TipoEmpresa objTipEmpObt= servicioTipoEmpresa.obtenerTipoEmpresa(codTipoEmpresa);
+
+	public String actualizaEmpresa() {
+
+		TipoEmpresa objTipEmpObt = servicioTipoEmpresa
+				.obtenerTipoEmpresa(codTipoEmpresa);
 		objEmpresa.setTipoEmpresa(objTipEmpObt);
 		servicioEmpresa.modificarEmpresa(objEmpresa);
-		lstEmpresa=servicioEmpresa.listar();
-		
+		lstEmpresa = servicioEmpresa.listar();
+
 		return "listaEmpresa";
 	}
 
 	public List<SelectItem> getLstTipoEmpresa() {
-        List<SelectItem> listaTipoEmpresa = new ArrayList();
-        lstTipoEmpresa=servicioTipoEmpresa.listar();
-        for (TipoEmpresa objTipoEmpresa : lstTipoEmpresa) {
-        	listaTipoEmpresa.add(new SelectItem(objTipoEmpresa.getCTipoEmpresa(),objTipoEmpresa.getNombre()));
-        }
-       
-        return listaTipoEmpresa;
-    }
-	
+		List<SelectItem> listaTipoEmpresa = new ArrayList();
+		lstTipoEmpresa = servicioTipoEmpresa.listar();
+		for (TipoEmpresa objTipoEmpresa : lstTipoEmpresa) {
+			listaTipoEmpresa.add(new SelectItem(objTipoEmpresa
+					.getCTipoEmpresa(), objTipoEmpresa.getNombre()));
+		}
 
-
-/*
-	public List<TipoEmpresa> getLstTipoEmpresa() {
-		return lstTipoEmpresa;
+		return listaTipoEmpresa;
 	}
-*/
 
+	/*
+	 * public List<TipoEmpresa> getLstTipoEmpresa() { return lstTipoEmpresa; }
+	 */
 
 	public void setLstTipoEmpresa(List<TipoEmpresa> lstTipoEmpresa) {
 		this.lstTipoEmpresa = lstTipoEmpresa;
 	}
-	
-	
 
 }
